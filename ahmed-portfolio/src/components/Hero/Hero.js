190624384cef15bd130d040ce4afaca710620b39
ahmed-shiiqaaye <me.shiqaye@gmail.com>
2022-05-10 21:40:51 +0200

    react-portfolio first change

---
+++ ahmed-portfolio/src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+      <main>
+          <div className="container">
+              <h2>Ahmed Abdirahman</h2>
+              <p className="job"> front-end developer | web developer.</p>
+              <button className="outline">Resume <i className="fa fa-file"></i></button>
+              <button className="outline">Projects </button>
+          </div>
+      </main>
+  )
+}
+
+export default Hero
---
+++ ahmed-portfolio/src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+function Navbar() {
+    const [click, setClick ] = useState(true)
+    const handleClick = () => setClick(!click)
+  return (
+    <header>
+        <div className="container">
+            <a href="#" onClick={handleClick} className="logo">
+                <img src="../img/Logo.png" alt="My logo image" />
+                <h4> Ahmed </h4>
+            </a>
+            <ul className={click ? 'nav_links active' : 'nav_links'} >
+                <li><a className='active' onClick={handleClick} href="#">Home</a></li>
+                <li><a onClick={handleClick} href="#">About</a></li>
+                <li><a onClick={handleClick} href="#">Projects</a></li>
+            </ul>
+            <div className="menu_icon" onClick={handleClick}>
+                <i className={click ? 'fa fa-times' : 'fa fa-bars'}></i>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
